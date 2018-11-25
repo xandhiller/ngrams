1546c856d2aac7aff73ba3ebf1efe67fa49bd1bc
xandhiller <xandhiller@gmail.com>
2018-11-26 10:56:06 +1100

    Playing around with lorem ipsum to get the paragraph of command line arguments to print out correctly in the terminal.

--- ngrams.py
+++ ngrams.py
@@ -1,7 +1,9 @@
 #! /usr/bin/python3
+lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 
 import pandas
 import sys
+from pprint import pprint
 
 ################################################################################
 # Parameters
@@ -15,28 +17,35 @@ n = 3
 ################################################################################
 
 #TODO:
-# class clArg:
-#   def __init__(self, argName, argSpec):
-#     self.argName = argName
-#     self.argSpec = argSpec
-#     # Limit the printing of the argSpec to 80 characters per line minus what is
-#     #   used on tabbing in the table.
-      
+class clArg:
+  def __init__(self, argName, argSpec):
+    self.argName = argName
+    self.argSpec = argSpec
+    # Limit the printing of the argSpec to 80 characters per line minus what is
+    #   used on tabbing in the table.
 
+  def argHelp(self):
+    name = self.argName
+    spec = self.argSpec
+
+    print(name + "\n")
+    print(lorem)  
+  
     
     
 ################################################################################
 # Functions
 ################################################################################
 def main():
-  print(sys.argv)
-
+  pass
 
 def helpClMsg():
-  print("\nngram.py command-line arguments:\n\n"
+  print("\n"
+        "ngram.py command-line arguments:\n\n"
         "-n\tChanges the ngram model to an integer specified.\n"
         "\tMaximum of 6. Must be an integer.\n\n"
         "-help\tRuns this message.\n")
+  nArg.argHelp()
         
 
 def commandLine():
@@ -52,6 +61,7 @@ def commandLine():
 
 
 if __name__ == "__main__":
+  nArg = clArg('n', lorem)
   if len(sys.argv) > 1: 
     commandLine()
   main()
